f73b92c078fdfa35d73005f0f9eec0df72c9c66c
Vinicius Santos <vinicius.vph@gmail.com>
2020-12-17 15:46:11 -0300

    configured globsl styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+// eslint-disable-next-line no-use-before-define
 import React from 'react';
 
-const App: React.FC = () => <h1>Hello World</h1>;
+import GlobalStyle from './styles/global';
 
+const App: React.FC = () => (
+  <>
+    <h1>Hello World</h1>;
+    <GlobalStyle />
+  </>
+);
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
+// eslint-disable-next-line no-use-before-define
 import React from 'react';
 import ReactDOM from 'react-dom';
+// eslint-disable-next-line import/extensions
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
-
