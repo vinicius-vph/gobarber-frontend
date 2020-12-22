41186bde61407d59123d7917f70cda41bc207933
Vinicius Santos <vinicius.vph@gmail.com>
2020-12-22 02:48:09 -0300

    made prettier adjustments

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -7,27 +7,29 @@ import logoImg from '../../assets/logo.svg';
 import { Container, Content, Background } from './styles';
 
 const SignIn: React.FC = () => (
-    <>
-        <Container>
-            <Content>
-                <img src={logoImg} alt="Gobarber" />
-                <form>
-                    <h1>Faça seu logon</h1>
+  <>
+    <Container>
+      <Content>
+        <img src={logoImg} alt="Gobarber" />
+        <form>
+          <h1>Faça seu logon</h1>
 
-                    <input placeholder="E-mail" />
+          <input placeholder="E-mail" />
 
-                    <input type="password" placeholder="Senha" />
+          <input type="password" placeholder="Senha" />
 
-                    <button type="submit">Entrar</button>
+          <button type="submit">Entrar</button>
 
-                    <a href="forgot">Esqueci minha senha</a>
-                </form>
+          <a href="forgot">Esqueci minha senha</a>
+        </form>
 
-                <a href=""><FiLogIn /> Criar conta</a>
-            </Content>
-            <Background />
-        </Container>
-    </>
+        <a href="">
+          <FiLogIn /> Criar conta
+        </a>
+      </Content>
+      <Background />
+    </Container>
+  </>
 );
 
 export default SignIn;
