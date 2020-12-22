95016637a6006d267aa6adf35bde813c13ff461a
Vinicius Santos <vinicius.vph@gmail.com>
2020-12-22 02:34:17 -0300

    conf eslint file | bugs fixes| signin page create

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-// eslint-disable-next-line no-use-before-define
 import React from 'react';
+
+import SignIn from './pages/SignIn';
 
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <h1>Hello World</h1>
+    <SignIn />
     <GlobalStyle />
   </>
 );
+
 export default App;
