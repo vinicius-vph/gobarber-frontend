f70eb613397c0a95de490c4eeb9eaee1e5260b7f
Vinicius Santos <vinicius.vph@gmail.com>
2020-12-29 16:24:58 -0300

    created alert toasts structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
+import ToastContainer from './components/ToastContainer';
 
 import GlobalStyle from './styles/global';
 
@@ -13,6 +14,7 @@ const App: React.FC = () => (
       <SignIn />
     </AuthProvider>
 
+    <ToastContainer />
     <GlobalStyle />
   </>
 );
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -52,11 +52,11 @@ const SignIn: React.FC = () => {
           password: data.password,
         });
       } catch (err) {
-        console.log(err);
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
 
-        const errors = getValidationErrors(err);
-
-        formRef.current?.setErrors(errors);
+          formRef.current?.setErrors(errors);
+        }
       }
     },
     [signIn],
