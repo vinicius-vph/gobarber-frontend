eb2f04b47cc42bf2910f90964a302f5f557d1590
Vinicius Santos <vinicius.vph@gmail.com>
2020-12-28 22:34:12 -0300

    created context api and login for context api

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,14 @@ import SignIn from './pages/SignIn';
 
 import GlobalStyle from './styles/global';
 
+import { AuthProvider } from './context/AuthContext';
+
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <AuthProvider>
+      <SignIn />
+    </AuthProvider>
+
     <GlobalStyle />
   </>
 );
