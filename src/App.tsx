815998a85ae00092678b16fde8ddcb077f98cc1c
Vinicius Santos <vinicius.vph@gmail.com>
2022-03-03 11:43:29 +0000

    app updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+
 import GlobalStyle from './styles/global';
+
 import AppProvider from './hooks';
+
 import Routes from './routes';
 
 const App: React.FC = () => (
